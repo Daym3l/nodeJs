4a4d841ca34e6d4aad319d51d1ed9a51916b5736
Daym3l <daymel537@gmail.com>
2019-10-04 19:11:43 -0400

    Section 8 finish

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,16 @@
 const path = require("path");
 
 const express = require("express");
-const productController = require("../controllers/controller.product");
+const adminController = require("../controllers/controller.admin");
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get("/add-product", productController.getAddProduct);
+router.get("/add-product", adminController.getAddProduct);
+
+router.get("/products", adminController.getProducts);
 
 // /admin/add-product => POST
-router.post("/add-product", productController.postAddProduct);
+router.post("/add-product", adminController.postAddProduct);
 
 module.exports = router;
-
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,18 @@ const path = require("path");
 
 const express = require("express");
 
-const controllerProducts = require("../controllers/controller.product");
+const controllerShop = require("../controllers/controller.shop");
 
 const router = express.Router();
 
-router.get("/", controllerProducts.getProducts);
+router.get("/", controllerShop.getIndex);
+
+router.get("/products", controllerShop.getProducts);
+
+router.get("/cart", controllerShop.getCart);
+
+router.get("/checkout", controllerShop.getCheckout);
+
+router.get('/orders', controllerShop.getOrders);
 
 module.exports = router;
