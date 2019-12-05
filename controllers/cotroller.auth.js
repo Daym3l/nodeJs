8264e5b93b9a8468508f753dca80dc964dc54ebc
Daym3l <daymel537@gmail.com>
2019-12-05 11:10:36 -0500

    class 10

--- controllers/cotroller.auth.js
+++ controllers/cotroller.auth.js
@@ -1,12 +1,13 @@
 exports.getLogin = (req, res, next) => {
+  let isLoggedIn = false;
+//  isLoggedIn = req.get("Cookie").split("=")[1];
   res.render("auth/login", {
     path: "/auth/login",
     pageTitle: "Login",
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: isLoggedIn
   });
 };
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  res.setHeader("set-Cookie", "loggedIn=true");
   res.redirect("/");
 };
- 
